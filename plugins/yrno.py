fa8f903bbcd0a62fbadf4c6d60bd0499d7f59fc4
IcEBnd <IcEBnd@github>
2012-07-03 10:36:23 +0200

    did a lot of fixes for yrno

--- plugins/yrno.py
+++ plugins/yrno.py
@@ -18,123 +18,127 @@ class YrNo(Command):
     def trig_yr(self, bot, source, target, trigger, argument, network, **kwargs):
         """ yr.no usage: location, [hour/date/"today"] """
 
-        argument = self.iso2utf8(argument)
+        try:
+            argument = self.iso2utf8(argument)
 
-        # Ugly hack FIXME
-        import signal
-        signal.alarm(20)                
+            # Ugly hack FIXME
+            import signal
+            signal.alarm(20)                
 
-        if not argument:
-            if source in self.places:
-                argument = self.places[source]
-            else:
-                argument = 'ryd'
+            if not argument:
+                if source in self.places:
+                    argument = self.places[source]
+                else:
+                    argument = 'ryd'
 
-        args = argument.split(",")
-        if len(args) > 1:
-            return "Not implemented, insert more beer"
+            args = argument.split(",")
+            if len(args) > 1:
+                return "Not implemented, insert more beer"
 
-        self.places[source] = argument
-        self.save()
+            self.places[source] = argument
+            self.save()
 
-        # Search for town
-        url = "http://www.yr.no/soek.aspx?sted=" + urllib2.quote(args[0])
-        request = urllib2.Request(url)
-        request.add_header("Cookie", "brp=spr=eng")
-        response = urllib2.urlopen(request)
+            # Search for town
+            url = "http://www.yr.no/soek.aspx?sted=" + urllib2.quote(args[0])
+            request = urllib2.Request(url)
+            request.add_header("Cookie", "brp=spr=eng")
+            response = urllib2.urlopen(request)
 
-        #print response.geturl()
+            #print response.geturl()
 
-        if response.geturl().find("http://www.yr.no/soek.aspx?sted=") != -1:
-            search = re.search('<a href="(/place/[^"]*)" title="[^"]*">', response.read())
+            if response.geturl().find("http://www.yr.no/soek.aspx?sted=") != -1:
+                search = re.search('<a href="(/place/[^"]*)" title="[^"]*">', response.read())
+                if search:
+                    url = "http://gammel.yr.no" + urllib2.quote(search.group(1))
+                    #print url
+                    request = urllib2.Request(url)
+                    request.add_header("Cookie", "brp=spr=eng")
+                    response = urllib2.urlopen(request)
+
+                    #print response.geturl()
+                else:
+                    return "Could not find any such place. Maybe you should move?"
+
+            # Parse overview page
+            stedbaseurl = response.geturl()
+
+            # Get Hour by Hour view
+            url = stedbaseurl + "hour_by_hour.html"
+            response = urllib2.urlopen(url)
+
+            #print stedbaseurl
+
+            search = re.search('\/([^\/]*)\/$', stedbaseurl)
             if search:
-                url = "http://gammel.yr.no" + urllib2.quote(search.group(1))
-                #print url
-                request = urllib2.Request(url)
-                request.add_header("Cookie", "brp=spr=eng")
-                response = urllib2.urlopen(request)
-
-                #print response.geturl()
+                town = urllib2.unquote(search.group(1))
             else:
-                return "Could not find any such place. Maybe you should move?"
-        
-        # Parse overview page
-        stedbaseurl = response.geturl()
-        
-        # Get Hour by Hour view
-        url = stedbaseurl + "hour_by_hour.html"
-        response = urllib2.urlopen(url)
+                town = "unknown"
 
-        #print stedbaseurl
+            data = response.read()
+            ofset = 0
+            hbh = {}
+            lhbh = []
 
-        search = re.search('\/([^\/]*)\/$', stedbaseurl)
-        if search:
-            town = urllib2.unquote(search.group(1))
-        else:
-            town = "unknown"
-     
-        data = response.read()
-        ofset = 0
-        hbh = {}
-        lhbh = []
-
-        for i in range(1, 100):
-            search = re.search('<th>([^ ]*) <strong>([^<]*)</strong></th>', data[ofset:])
-            if search:
-                ofset += search.end()
-                #print "'%s' '%s'" % (search.group(1), search.group(2))
-                key = search.group(1) + " " + search.group(2)
-                hbh[key] = {}
-                hbh[key]['day'] = search.group(1)
-                hbh[key]['time'] = search.group(2)
-
-                # FIXME should add end here
-
-                # Search for all properties
-                search = re.search('<td title="([^"]*)">', data[ofset:])
+            for i in range(1, 100):
+                search = re.search('<th>([^ ]*) <strong>([^<]*)</strong></th>', data[ofset:])
                 if search:
-                    hbh[key]['weather'] = search.group(1)
+                    ofset += search.end()
+                    #print "'%s' '%s'" % (search.group(1), search.group(2))
+                    key = search.group(1) + " " + search.group(2)
+                    hbh[key] = {}
+                    hbh[key]['day'] = search.group(1)
+                    hbh[key]['time'] = search.group(2)
 
-                search = re.search('<td class="precipitation">([0-9\.]*) mm</td>', data[ofset:])
-                if search:
-                    hbh[key]['precipitation'] = search.group(1)
+                    # FIXME should add end here
 
-                search = re.search('<td class="(plus|minus)"[^>]*>([\-0-9]*)[^<]*</td>', data[ofset:])
-                if search:
-                    hbh[key]['temp'] = search.group(2)
+                    # Search for all properties
+                    search = re.search('<td title="([^"]*)">', data[ofset:])
+                    if search:
+                        hbh[key]['weather'] = search.group(1)
 
-                search = re.search('<img src="[^"]*" title="([^,]*), ([0-9]*) m/s from ([^"]*)"', data[ofset:])
-                if search:
-                    hbh[key]['wind'] = search.group(1)
-                    hbh[key]['wind_strength'] = search.group(2)
-                    hbh[key]['wind_direction'] = search.group(3)
+                    search = re.search('<td class="precipitation">([0-9\.]*) mm</td>', data[ofset:])
+                    if search:
+                        hbh[key]['precipitation'] = search.group(1)
 
-                search = re.search('<td class="pressure">([^<]*)</td>', data[ofset:])
-                if search:
-                    hbh[key]['pressure'] = search.group(1)
+                    search = re.search('<td class="(plus|minus)"[^>]*>([\-0-9]*)[^<]*</td>', data[ofset:])
+                    if search:
+                        hbh[key]['temp'] = search.group(2)
 
-                lhbh.append(hbh[key])
-            else:
-                break
+                    search = re.search('<img src="[^"]*" title="([^,]*), ([0-9]*) m/s from ([^"]*)"', data[ofset:])
+                    if search:
+                        hbh[key]['wind'] = search.group(1)
+                        hbh[key]['wind_strength'] = search.group(2)
+                        hbh[key]['wind_direction'] = search.group(3)
 
-        #print hbh
-#        for x in hbh.items():
-#            print x
+                    search = re.search('<td class="pressure">([^<]*)</td>', data[ofset:])
+                    if search:
+                        hbh[key]['pressure'] = search.group(1)
 
-        result = "Weather in %s at " % town
-        for t in lhbh[:4]:
-            result += "%s: %s %sC %s mm %s m/s %s " % (t.get("time", "N/A"), t.get("weather", "N/A"), t.get("temp", "N/A"), t.get("precipitation", "N/A"), t.get("wind_strength", "N/A"), t.get("wind_direction", "N/A"))
+                    lhbh.append(hbh[key])
+                else:
+                    break
 
-        # Find rain!
-        rains = ""
-        for t in lhbh:
-            if float(t.get("precipitation", 0.11)) > 0.1:
-                rains += "%s%s %smm " % (t["day"][:2], t.get("time", "N/A"), t.get("precipitation", "N/A"))
+            #print hbh
+    #        for x in hbh.items():
+    #            print x
 
-        if len(rains) != 0:
-            result += "DANGER rain! " + rains
+            result = "Weather in %s at " % town
+            for t in lhbh[:4]:
+                result += "%s: %s %sC %s mm %s m/s %s " % (t.get("time", "N/A"), t.get("weather", "N/A"), t.get("temp", "N/A"), t.get("precipitation", "N/A"), t.get("wind_strength", "N/A"), t.get("wind_direction", "N/A"))
 
-        return result
+            # Find rain!
+            rains = ""
+            for t in lhbh:
+                if float(t.get("precipitation", 0.11)) > 0.1:
+                    rains += "%s%s %smm " % (t["day"][:2], t.get("time", "N/A"), t.get("precipitation", "N/A"))
+
+            if len(rains) != 0:
+                result += "DANGER rain! " + rains
+                
+            return result
+
+        except urllib2.HTTPError, ex:
+            return "yr.no returned: " + str(ex)
 
     def iso2utf8(self, s):
         try:
@@ -170,5 +174,6 @@ class YrNo(Command):
 #print YrNo().trig_yr(None, None, None, None, "Link\xf6ping", None)
 #print YrNo().trig_yr(None, None, None, None, "Link\xc3\xb6ping", None)
 #print YrNo().trig_yr(None, None, None, None, "Linkoping", None)
+#print YrNo().trig_yr(None, None, None, None, "Stockholm", None)
 #print YrNo().trig_yr(None, None, None, None, "Kiruna", None)
 #print YrNo().trig_yr(None, None, None, None, "Uppsala", None)
