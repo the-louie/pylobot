59ca0b10eb944e4f3219d0b6115a7767cdac9c51
Anders Green <anders.green@proceranetworks.com>
2014-06-08 17:02:47 +0200

    removed unused plugins

--- plugins/sayaao.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-from commands import Command
-class SayAAOCommand(Command):
-	
-	def trig_sayaao(self, bot, source, target, trigger, argument, network):
-		bot.tell(network, target, u"Detta är UTF-8 åäöÅÄÖ.".encode("UTF-8"))
-		bot.tell(network, target, u"och detta är ISO-8859-1 åäöÅÄÖ.".encode("ISO-8859-1"))
-		return None
