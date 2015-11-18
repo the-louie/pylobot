3442515207fbe82fa1df5976f6f17dd31dbf4aa9
Anders Green <anders.green@proceranetworks.com>
2015-11-18 07:56:15 +0100

    checked value of wrong deferred_join_all
    Looked at the config value instead of the value changed by
    the client. Added some logging to the autojoin plugin also.

--- plugins/autojoin.py
+++ plugins/autojoin.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 from commands import Command
 import datetime
+import logging
+logger = logging.getLogger('landlady')
 
 class AutoJoin(Command):
 	def __init__(self):
@@ -11,12 +13,16 @@ class AutoJoin(Command):
 		self.join_all_channels()
 
 	def join_all_channels(self):
-		if self.bot.settings.deferred_join_all:
+		if self.bot.client.deferred_join_all:
+			logger.debug("(AutoJoin:join_all_channels) Joining defered, checking in 10s.")
 			self.bot.add_timer(datetime.timedelta(seconds=10), False, self.join_all_channels)
 			return
 
+		logger.debug("(AutoJoin:join_all_channels) Joining channels:")
 		for channel in self.bot.settings.server['channels']:
 			if len(channel) == 2:
+				logger.debug("(AutoJoin:join_all_channels)\t* %s (%s)" % (channel[0], channel[1]))
 				self.bot.join(channel[0], channel[1])
 			elif len(channel) == 1:
+				logger.debug("(AutoJoin:join_all_channels)\t* %s" % (channel[0],))
 				self.bot.join(channel[0])
